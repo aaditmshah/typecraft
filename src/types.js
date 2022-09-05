82191833c1f691a162fc6a249fdc365138b5980e
Aadit M Shah <aaditmshah@aadit.codes>
2022-09-05 14:35:40 +0530

    feat: rename recursive to spin and make it dry

--- src/types.js
+++ src/types.js
@@ -1,3 +1,6 @@
+const add = (set, item) => new Set([...set, item]);
+const remove = (set, item) => new Set([...set].filter((data) => data !== item));
+
 const unknown = { tag: "unknown" };
 const never = { tag: "never" };
 const string = { tag: "string" };
@@ -16,12 +19,11 @@ const union = (...types) => ({ tag: "union", types });
 const intersection = (...types) => ({ tag: "intersection", types });
 const pure = (value) => ({ tag: "pure", value });
 const map = (morphism, type) => ({ tag: "map", morphism, type });
+const reference = (name) => ({ tag: "reference", symbol: name });
+const recursive = (name, type) => ({ tag: "recursive", symbol: name, type });
 const fix = (combinators) => {
   const propTypes = Object.fromEntries(
-    Object.keys(combinators).map((key) => [
-      key,
-      { tag: "reference", symbol: Symbol(key) }
-    ])
+    Object.keys(combinators).map((key) => [key, reference(Symbol(key))])
   );
   const context = Object.fromEntries(
     Object.entries(combinators).map(([key, combinator]) => [
@@ -32,12 +34,12 @@ const fix = (combinators) => {
   return Object.fromEntries(
     Object.keys(combinators).map((key) => [
       key,
-      recursive(context, new Set(), propTypes[key]).type
+      spin(context, new Set(), propTypes[key]).type
     ])
   );
 };
 
-function recursive(context, environment, inputType) {
+function spin(context, environment, inputType) {
   switch (inputType.tag) {
     case "unknown":
     case "never":
@@ -48,78 +50,76 @@ function recursive(context, environment, inputType) {
     case "symbol":
       return { type: inputType, free: new Set() };
     case "array": {
-      const { type, free } = recursive(context, environment, inputType.type);
+      const { type, free } = spin(context, environment, inputType.type);
       return { type: array(type), free };
     }
     case "tuple": {
-      const { types, free } = concat(context, environment, inputType.types);
+      const { types, free } = spinAll(context, environment, inputType.types);
       return { type: tuple(types), free };
     }
     case "record": {
-      const { type, free } = recursive(context, environment, inputType.type);
+      const { type, free } = spin(context, environment, inputType.type);
       return { type: record(type), free };
     }
     case "object": {
       const set = new Set();
       const propTypes = Object.fromEntries(
         Object.entries(inputType.propTypes).map(([key, propertyType]) => {
-          const { type, free } = recursive(context, environment, propertyType);
-          for (const reference of free) set.add(reference);
+          const { type, free } = spin(context, environment, propertyType);
+          for (const name of free) set.add(name);
           return [key, type];
         })
       );
       return { type: object(propTypes), free: set };
     }
     case "nullable": {
-      const { type, free } = recursive(context, environment, inputType.type);
+      const { type, free } = spin(context, environment, inputType.type);
       return { type: nullable(type), free };
     }
     case "optional": {
-      const { type, free } = recursive(context, environment, inputType.type);
+      const { type, free } = spin(context, environment, inputType.type);
       return { type: optional(type), free };
     }
     case "enumeration":
       return { type: inputType, free: new Set() };
     case "union": {
-      const { types, free } = concat(context, environment, inputType.types);
+      const { types, free } = spinAll(context, environment, inputType.types);
       return { type: union(types), free };
     }
     case "intersection": {
-      const { types, free } = concat(context, environment, inputType.types);
+      const { types, free } = spinAll(context, environment, inputType.types);
       return { type: intersection(types), free };
     }
     case "pure":
       return { type: inputType, free: new Set() };
     case "map": {
-      const { type, free } = recursive(context, environment, inputType.type);
+      const { type, free } = spin(context, environment, inputType.type);
       return { type: map(inputType.morphism, type), free };
     }
     case "reference": {
-      const reference = inputType.symbol;
-      if (!Object.hasOwn(context, reference) || environment.has(reference))
-        return { type: inputType, free: new Set([reference]) };
-      const extended = new Set([...environment, reference]);
-      const { type, free } = recursive(context, extended, context[reference]);
-      if (!free.has(reference)) return { type, free };
-      const set = new Set([...free].filter((name) => name !== reference));
-      return { type: { tag: "recursive", symbol: reference, type }, free: set };
+      const name = inputType.symbol;
+      return Object.hasOwn(context, name) && !environment.has(name)
+        ? spin(context, environment, recursive(name, context[name]))
+        : { type: inputType, free: new Set([name]) };
     }
     case "recursive": {
-      const reference = inputType.symbol;
-      const extended = new Set([...environment, reference]);
-      const { type, free } = recursive(context, extended, inputType.type);
-      return { type: { tag: "recursive", symbol: reference, type }, free };
+      const name = inputType.symbol;
+      const result = spin(context, add(environment, name), inputType.type);
+      const { type, free } = result;
+      return free.has(name)
+        ? { type: recursive(name, type), free: remove(free, name) }
+        : result;
     }
     default:
       throw new TypeError(`unknown type ${inputType}`);
   }
 }
 
-function concat(context, environment, inputTypes) {
+function spinAll(context, environment, inputTypes) {
   const set = new Set();
   const types = inputTypes.map((inputType) => {
-    const { type, free } = recursive(context, environment, inputType);
-    for (const reference of free) set.add(reference);
+    const { type, free } = spin(context, environment, inputType);
+    for (const name of free) set.add(name);
     return type;
   });
   return { types, free: set };
