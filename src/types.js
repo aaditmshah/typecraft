483dd40bdcd44ec10404b645f5d1ae85e945707f
Aadit M Shah <aaditmshah@aadit.codes>
2022-09-18 09:46:24 +0530

    :sparkles: Create a Yoneda embedding for type schemas.

--- src/types.js
+++ src/types.js
@@ -1,27 +1,45 @@
-const unknown = { tag: "unknown" };
-const never = { tag: "never" };
-const primitive = (type) => ({ tag: "primitive", type });
+import { nil, cons } from "./pipe";
+
+const unknown = { tag: "unknown", composition: nil() };
+const never = { tag: "never", composition: nil() };
+const primitive = (type) => ({ tag: "primitive", type, composition: nil() });
 const string = primitive("string");
 const number = primitive("number");
 const bigint = primitive("bigint");
 const boolean = primitive("boolean");
 const symbol = primitive("symbol");
-const array = (type) => ({ tag: "array", type });
-const tuple = (...types) => ({ tag: "tuple", types });
-const record = (type) => ({ tag: "record", type });
-const object = (propTypes) => ({ tag: "object", propTypes });
+const array = (type) => ({ tag: "array", type, composition: nil() });
+const tuple = (...types) => ({ tag: "tuple", types, composition: nil() });
+const record = (type) => ({ tag: "record", type, composition: nil() });
+const object = (propTypes) => ({
+  tag: "object",
+  propTypes,
+  composition: nil()
+});
 const enumeration = (...values) => ({
   tag: "enumeration",
-  values: new Set(values)
+  values: new Set(values),
+  composition: nil()
 });
-const union = (...types) => ({ tag: "union", types });
+const union = (...types) => ({ tag: "union", types, composition: nil() });
 const nullable = (type) => union(type, primitive("null"));
 const optional = (type) => union(type, primitive("undefined"));
-const intersection = (...types) => ({ tag: "intersection", types });
-const pure = (value) => ({ tag: "pure", value });
-const map = (morphism, type) => ({ tag: "map", morphism, type });
+const intersection = (...types) => ({
+  tag: "intersection",
+  types,
+  composition: nil()
+});
+const pure = (value) => ({ tag: "pure", value, composition: nil() });
+const map = (morphism, type) => ({
+  ...type,
+  composition: cons(morphism, type.composition)
+});
 const fix = (combinator) => {
-  const type = combinator({ tag: "lazy", getType: () => type });
+  const type = combinator({
+    tag: "lazy",
+    getType: () => type,
+    composition: nil()
+  });
   return type;
 };
 
